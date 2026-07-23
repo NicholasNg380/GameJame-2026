e2a0ba630c80b0c5521688fb8d2fc69ce3f5aec3
Nicholas Ng <nnyc380@outlook.com>
2026-07-23 20:06:37 +1000

    Add melee enemy scene and chase behavior
    Instantiate Enemy in World and add a melee enemy scene with basic chase logic. Summary of changes: World.tscn adds an Enemy instance; Scenes/Objects/Enemy.tscn switched the script to Melee_enemy_script, renamed the sprite node to Sword, adjusted rotation, changed default animation and increased walking animation speed; Scripts/Enemy/Enemy_script.gd now declares class_name Enemy, adds enemies to a group, finds the player, prints a debug line, reduces chase velocity (300 -> 100), uses move_and_slide() and calls look_at(player); Melee_player_script.gd was renamed/moved to Scripts/Enemy/Melee_enemy_script.gd, now extends Enemy_script.gd, gets the Sword AnimatedSprite onready and plays the Walking animation. UID file renamed accordingly.

--- Scripts/Enemy/Enemy_script.gd
+++ Scripts/Enemy/Enemy_script.gd
@@ -1,14 +1,16 @@
 extends CharacterBody2D
+class_name Enemy
 
 var player: Player
 
 func _ready():
 	add_to_group("enemies")
+	print("test")
 	player = get_tree().get_first_node_in_group("player")
-	print(player)
 
 func _physics_process(_delta):
 	var direction = global_position.direction_to(player.global_position)
 		
-	velocity = direction * 300
+	velocity = direction * 100
 	move_and_slide()
+	look_at(player.global_position)
