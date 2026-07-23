943e921bb7666f031e8c2576a07e21f4787e2c81
Nicholas Ng <nnyc380@outlook.com>
2026-07-23 12:54:16 +1000

    Player movement and camera

---
+++ Scripts/Player_script.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+const ACCELERATION: int = 15
+const FRICTION: int = 0
+var MAX_SPEED: float = 500.0
+
+func _physics_process(delta):
+	_movement(delta)
+	move_and_slide()
+
+func _movement(delta: float) -> void:
+	var input = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	).normalized()
+	
+	var lerp_weight = delta * (ACCELERATION if input else 50)
+	
+	velocity = lerp(velocity, input * (MAX_SPEED), lerp_weight)
