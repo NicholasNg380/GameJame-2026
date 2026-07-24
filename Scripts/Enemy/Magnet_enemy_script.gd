b84e5957f075ec8cd166f59d6775cff1d4a00bb4
Joshua Cranitch <joshua.cranitch@gmail.com>
2026-07-24 17:44:35 +1000

    Revert "Merge branch 'main' into josh"
    This reverts commit 74ddd3de8f4bfd0f37c9c93b1d43c6a0efa78aaa, reversing
    changes made to 0962d9bd744d0533aaeb3eae887840b52d2745df.

--- Scripts/Enemy/Magnet_enemy_script.gd
+++ Scripts/Enemy/Magnet_enemy_script.gd
@@ -1,12 +1,17 @@
 extends "Enemy_script.gd"
 
+@onready var anim = $Magnet
+
+
+
 var type = "Magnet"
 
-func _ready():
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
 	super()
+	anim.play("Walking")
 
-func _on_animated_sprite_2d_animation_finished() -> void:
-	if anim.animation == "Waking up":
-		spawning = false
-		on = true
-		anim.play("Walking")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- Scripts/Enemy/Tank_enemy_script.gd
+++ Scripts/Enemy/Tank_enemy_script.gd
@@ -1,13 +1,15 @@
 extends "Enemy_script.gd"
 
+@onready var anim = $Tank
+
 var type = "Tank"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super()
+	anim.play("Walking")
 
-func _on_animated_sprite_2d_animation_finished() -> void:
-	if anim.animation == "Waking up":
-		spawning = false
-		on = true
-		anim.play("Walking")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
