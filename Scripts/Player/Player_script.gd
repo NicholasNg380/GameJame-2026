0b3cb961d788d2ec32579961535c07399784415e
Nicholas Ng <nnyc380@outlook.com>
2026-07-24 14:28:49 +1000

    Add Magnet enemy type and improve AI behavior
    Introduce a new Magnet enemy type with sprite animations and implement state-based AI system (patrol/chase) using NavigationAgent2D. Add Magnet robot form to player with sprite animations. Improve camera smoothing responsiveness and streamline player state management during robot hacking.

--- Scripts/Player/Player_script.gd
+++ Scripts/Player/Player_script.gd
@@ -5,6 +5,7 @@ class_name Player
 @onready var camera = $Camera2D
 @onready var sword_anim = $Sword
 @onready var tank_anim = $Tank
+@onready var magnet_anim = $Magnet
 
 const ACCELERATION: int = 15
 const FRICTION: int = 0
@@ -15,15 +16,11 @@ var HEALTH: float = 100.0
 # Array contains Health then speed
 var ROBOTS: Dictionary = {"Sword": [100.0, 500.0], "Tank": [200.0, 250.0], "Magnet": [50.0, 750.0]}
 
-var can_move: bool = true
 
 func _ready():
 	add_to_group("player")
 
 func _physics_process(delta):
-	if not can_move:
-		return
-	
 	_movement(delta)
 	move_and_slide()
 	
@@ -76,15 +73,14 @@ func hack_robot():
 	robot_change(robot.type)
 	
 	camera.position_smoothing_enabled = true
-	can_move = false
 	
 	global_position = robot_pos
 	robot.global_position = player_pos
 	
 	robot.queue_free()
-	await get_tree().create_timer(1.75).timeout
+	await get_tree().create_timer(0.3).timeout
 	camera.position_smoothing_enabled = false
-	can_move = true
+
 
 func robot_change(type) -> void:
 	TYPE = type
@@ -93,9 +89,12 @@ func robot_change(type) -> void:
 	
 	sword_anim.visible = false
 	tank_anim.visible = false
+	magnet_anim.visible = false
 	match type:
 		"Sword":
 			sword_anim.visible = true
 		"Tank":
 			tank_anim.visible = true
+		"Magnet":
+			magnet_anim.visible = true
 	
