826bfea224280129bab7a925c2a71f8162d465eb
Bates3 <benjaminjbateman@gmail.com>
2026-07-25 18:36:40 +1000

    added magnet bomb thing

--- Scenes/Objects/magnet_bomb.gd
+++ Scenes/Objects/magnet_bomb.gd
@@ -1,41 +1,42 @@
 extends StaticBody2D
 
-var INIT_VELOCITY: float = 200
+@export var INIT_VELOCITY: float = 1000.0
 
 var current_velocity := 0.0
-var friction := 80.0
+var friction := 450.0
+var direction := Vector2.ZERO
 
 const ANIM_TIMER_CONST := 1.0
 var animate_timer := 0.0
 
 @onready var light = $PointLight2D
 @onready var anim = $AnimatedSprite2D
-@onready var collision = $CollisionShape2D
+@onready var collision = $Area2D/CollisionShape2D
 
 func _ready() -> void:
-	print("HELLO")
-	
 	collision.disabled = true
 	current_velocity = INIT_VELOCITY
 	animate_timer = ANIM_TIMER_CONST
 	anim.play("default")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if current_velocity > 0:
-		current_velocity -= friction
+		current_velocity = max(current_velocity - friction * delta, 0)
+		global_position += direction * current_velocity * delta
 	else:
-		current_velocity = 0
-		if animate_timer == 0:
+		if animate_timer <= 0:
 			animate_timer = ANIM_TIMER_CONST
 			light.visible = !light.visible
+
 	animate_timer -= delta
-	
+
+func launch(start_pos: Vector2, dir: Vector2) -> void:
+	global_position = start_pos
+	direction = dir.normalized()
+	rotation = direction.angle() # Optional: rotate to face travel direction
+
 func stop_object():
 	current_velocity = 0
 
 func activate():
 	collision.disabled = false
-	
-	
